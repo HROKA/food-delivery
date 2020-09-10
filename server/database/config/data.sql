d4eb2575019e698524aae9d95e6b44160ceec2c4
ahmadysalah <a.ahmad.salah@gmail.com>
2020-09-10 20:03:09 +0300

    create fakedata and fix bugs in relations between create databse and initialize  data #6

--- server/database/config/data.sql
+++ server/database/config/data.sql
@@ -1,16 +1,19 @@
 BEGIN;
-DROP TABLE IF EXISTS admins,
+  DROP TABLE IF EXISTS admins,
 products,
 clients,
 orders,
-details CASCADE;
-CREATE TABLE admins (
+details
+  CASCADE;
+CREATE TABLE admins
+(
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL UNIQUE,
   password TEXT NOT NULL,
   rules TEXT NOT NULL
 );
-CREATE TABLE products (
+CREATE TABLE products
+(
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL UNIQUE,
   unit TEXT NOT NULL,
@@ -18,36 +21,43 @@ CREATE TABLE products (
   image TEXT NOT NULL,
   discount_value INTEGER DEFAULT 0,
   available Boolean DEFAULT true,
-  category TEXT [] NOT NULL
+  category TEXT
+  [] NOT NULL
 );
-CREATE TABLE clients (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  mobile_number TEXT NOT NULL,
-  facebook_profile TEXT,
-  password TEXT NOT NULL,
-  avatar TEXT Not NUll,
-  location TEXT,
-  address TEXT,
-  favorite TEXT []
+  CREATE TABLE clients
+  (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    mobile_number TEXT NOT NULL,
+    facebook_profile TEXT,
+    password TEXT NOT NULL,
+    avatar TEXT Not NUll,
+    location TEXT,
+    address TEXT,
+    favorite TEXT
+    []
 );
-CREATE TABLE orders (
-  id SERIAL PRIMARY KEY,
-  client_id INTEGER NOT NULL REFERENCES clients(id),
-  date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  total INTEGER NOT NULL,
-  order_price INTEGER NOT NULL,
-  status VARCHAR(255) NOT NULL DEFAULT 'pending',
-  address TEXT NOT NULL,
-  mobile_number TEXT NOT NULL,
-  dilivery_price FLOAT NOT NULL
-);
-CREATE TABLE details (
-  id SERIAL PRIMARY KEY,
-  product_id INTEGER NOT NULL REFERENCES products(id),
-  quantity INTEGER NOT NULL,
-  price FLOAT NOT NULL,
-  Total_price FLOAT NOT NULL,
-  order_id INTEGER NOT NULL REFERENCES orders(id)
-);
-COMMIT;
+    CREATE TABLE orders
+    (
+      id SERIAL PRIMARY KEY,
+      client_id INTEGER NOT NULL REFERENCES clients(id),
+      date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+      total INTEGER NOT NULL,
+      order_price INTEGER NOT NULL,
+      status VARCHAR(255) NOT NULL DEFAULT 'pending',
+      address TEXT NOT NULL,
+      mobile_number TEXT NOT NULL,
+      dilivery_price FLOAT NOT NULL
+    );
+    CREATE TABLE details
+    (
+      id SERIAL PRIMARY KEY,
+      product_id INTEGER NOT NULL REFERENCES products(id),
+      quantity INTEGER NOT NULL,
+      price FLOAT NOT NULL,
+      Total_price FLOAT NOT NULL,
+      order_id INTEGER NOT NULL REFERENCES orders(id)
+    );
+
+
+    COMMIT;
