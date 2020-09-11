e1e70e6a05ac65dda96c76c27e85a9152f85e39c
ahmadysalah <a.ahmad.salah@gmail.com>
2020-09-11 21:47:33 +0300

    delete product by id [Admin] - create sql query - create function - create admin route #12

--- server/controllers/routes/products.js
+++ server/controllers/routes/products.js
@@ -1,10 +1,26 @@
-const { getAllProducts } = require('../../database/queries/products');
+const {
+  getAllProducts,
+  deleteProduct,
+} = require('../../database/queries/products');
 
 const products = {
+  // get all products
   getAllProducts: (req, res, next) =>
     getAllProducts()
       .then(({ rows }) => res.status(200).json(rows))
-      .catch(next),
+      .catch(next('failed to get products')),
+
+  // delete product with id
+  deleteProduct: (req, res, next) => {
+    const { id } = req.params;
+    deleteProduct(id)
+      .then(() =>
+        res
+          .status(200)
+          .json({ message: `Product ${id} was deleted successfully` })
+      )
+      .catch(next('Product has been used in orders'));
+  },
 };
 
 module.exports = products;
--- server/router/admin.js
+++ server/router/admin.js
@@ -1,8 +1,17 @@
 const adminRouter = require('express').Router();
 
 const { AllOrders, deleteOrderbyId } = require('../controllers/routes/orders');
+const {
+  getAllProducts,
+  deleteProduct,
+} = require('../controllers/routes/products');
 
 adminRouter.get('/orders', AllOrders);
+
 adminRouter.delete('/orders/:id', deleteOrderbyId);
+
+adminRouter.get('/products', getAllProducts);
+
+adminRouter.delete('/product/:id', deleteProduct);
 
 module.exports = adminRouter;
