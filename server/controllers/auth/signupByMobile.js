7f39b8267fc6159885c04c304a33c2292c7f6155
hassanannajjar <hassan.an.najjar@gmail.com>
2020-09-24 02:20:46 +0300

    added try for all code

--- server/controllers/auth/signupByMobile.js
+++ server/controllers/auth/signupByMobile.js
@@ -7,26 +7,25 @@ const { getClient_Data } = require('../../database/queries/profile');
 
 const signUp = async (req, res, next) => {
   const { name, password, mobile_number, location, address } = req.body;
-
   // get random color to use it in avatar
   const randomColor = Math.random().toString(16).substr(-6);
   // generation avatar with client name
   const avatar = `https://ui-avatars.com/api/?name=${name}&rounded=true&background=${randomColor}&color=F3F3F3`;
   const favorite = [];
 
-  // check if mobile number is exist
-  const checkMobileNumber = await getClient_Data({ mobile_number });
+  try {
+    // check if mobile number is exist
+    const checkMobileNumber = await getClient_Data({ mobile_number });
 
-  if (checkMobileNumber.rowCount > 0) {
-    res.status(400).json({ message: 'this account is already exist' });
-  } else {
-    // hash password
-    const hashedPassword = await hash(password, 10);
-    // add the client data to use it as client token
-    const clientData = name + hashedPassword;
-    const clientToken = sign(clientData, process.env.SECRET_KEY);
+    if (checkMobileNumber.rowCount > 0) {
+      res.status(400).json({ message: 'this account is already exist' });
+    } else {
+      // hash password
+      const hashedPassword = await hash(password, 10);
+      // add the client data to use it as client token
 
-    try {
+      const clientData = name + hashedPassword;
+      const clientToken = sign(clientData, process.env.SECRET_KEY);
       await signUpByMobile(
         name,
         hashedPassword,
@@ -40,9 +39,9 @@ const signUp = async (req, res, next) => {
       res
         .status(200)
         .json({ message: ' signUp successful', role: clientToken });
-    } catch (error) {
-      next('signUp field');
     }
+  } catch (error) {
+    next('signUp field');
   }
 };
 
